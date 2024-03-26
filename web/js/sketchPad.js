4c9683a2598ab54b4e151e598e6d39b5298e4ed8
isomorpheric <ecam900@gmail.com>
2024-03-25 23:36:04 -0400

    feat(in-progress): add save button

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -18,12 +18,15 @@ class SketchPad {
 
     this.ctx = this.canvas.getContext('2d');
 
-    this.paths = [];
-    this.isDrawing = false;
-
-    this.#redraw();
+    this.reset();
 
     this.#addEventListeners();
+  }
+
+  reset() {
+    this.paths = [];
+    this.isDrawing = false;
+    this.#redraw();
   }
 
   #addEventListeners() {
